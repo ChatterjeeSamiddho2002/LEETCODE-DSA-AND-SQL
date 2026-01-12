fd83001c07d56be6def0713b5334259a2aa10528
Samiddho Chatterjee <137328265+ChatterjeeSamiddho2002@users.noreply.github.com>
2026-01-12 09:30:49 +0530

    Time: 0 ms (100%), Space: 9.8 MB (55.24%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.c
@@ -0,0 +1,15 @@
+int minTimeToVisitAllPoints(int** points, int pointsSize, int* pointsColSize) {
+    int time=0;
+    for (int i = 1; i < pointsSize; i++) {
+        int dx = abs(points[i][0] - points[i - 1][0]);
+        int dy = abs(points[i][1] - points[i - 1][1]);
+
+        if (dx > dy) {
+            time += dx;
+        } else {
+            time += dy;
+        }
+    }
+
+    return time;
+}
